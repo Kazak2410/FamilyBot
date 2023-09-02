a7a62ca70b0b1ed4b6de2eeff145173eafbb7be1
Ilya <ballukrainian@gmail.com>
2023-09-02 17:48:30 +0300

    initial commit

---
+++ db.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+
+class DataBase:
+    def __init__(self, filename):
+        self.connection = sqlite3.connect(f"{filename}.db")
+        self.cursor = self.connection.cursor()
+
+    def create_table(self):
+        self.cursor.execute("""
+            CREATE TABLE users (
+                id INTEGER PRIMARY KEY,
+                name TEXT NOT NULL
+            )
+        """)
+
+    def insert_user(self, user):
+        self.cursor.execute("""
+            INSERT OR IGNORE INTO users (id, name) VALUES (?, ?)
+        """, (user.from_user.id, user.from_user.username))
+        self.connection.commit()
+
+    def get_users(self):
+        self.cursor.execute("""
+            SELECT name FROM users
+        """)
+        return " ".join(name[0] for name in self.cursor.fetchall())
+
+    def check_table(self):
+        self.cursor.execute("SELECT EXISTS(SELECT 1 FROM sqlite_master WHERE type='table' AND name='users')")
+        exists = self.cursor.fetchone()[0]
+        return exists
---
+++ main.py
@@ -0,0 +1,63 @@
+import os
+from datetime import datetime, timedelta
+import asyncio
+from db import DataBase
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher, executor, types
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+
+load_dotenv()
+
+bot = Bot(os.getenv("TOKEN"))
+db = Dispatcher(bot)
+database = DataBase("family")
+number = 0
+
+
+def get_users():
+    users = database.get_users().split()
+    return users
+
+async def send_message_cron(bot: Bot):
+    global number
+    if number < len(get_users()):
+        user = get_users()[number]
+    else:
+        number = 0
+        user = get_users()[number]
+    number += 1
+    message = f"Сегодня дежурит на кухне @{user}"
+    await bot.send_message(-945893857, message)
+
+
+scheduler = AsyncIOScheduler(timezone="Europe/Kyiv")
+scheduler.add_job(send_message_cron, trigger="cron", hour=17, minute=23,
+                  start_date=datetime.now(), kwargs={"bot": bot})
+scheduler.start()
+
+
+async def on_startup(_):
+    if not database.check_table():
+        database.create_table()
+    print("The bot has been connected!")
+
+
+@db.message_handler(commands=['start'])
+async def cmd_start(message: types.Message):
+    await message.answer('Поздравляем ты присоединился!')
+    print(get_users())
+    database.insert_user(message)
+
+
+@db.message_handler(commands=["list"])
+async def cmd_list(message: types.Message):
+    await message.answer(database.get_users())
+
+
+if __name__ == "__main__":
+    # loop = asyncio.get_event_loop()
+    # loop.create_task(scheduled(5))
+    executor.start_polling(db, on_startup=on_startup, skip_updates=True)
+
+""" Остановился на функции, которая должна поочередно выдавать имена пользователей, с интревалом раз в день"""
